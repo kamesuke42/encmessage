33c85d3dec84b65367c01ab4ac3008bb682d408c
kamesuke42 <4amesuke42@gmail.com>
2023-09-13 17:34:58 +0900

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,68 @@
+const publicKeyArmored = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQGNBGUBc04BDADD6PIkZifzrZIxIWNVe/ktiBF3uzoycGHeA3pm1IegolpknONs
+dmw52+g4Up6Ijl/ZGCrWF3+KrUp35wKSv7DwNv7ib5Qr5vFoAV5Pb9MY3kGV0OCA
+BXCyYv1z6x2/omKpgsaMPPa0WA81/EeULzp98b7fbyxrJjfHpCFAn0JKpXE0lITD
+WZRnmvZFxT5PkCJY0hL/yL3kgcatb3Aj8Mooydt3wv1lCndSFeAUjGapEvd850on
+quLYqkKZJdxJJsGeJakt1v9cwfXUkCbSgvXMUxJlZ4TtUBqEYG/29whvZocjlwRj
+rKhVqyQUfF74r9b85+BxHB7y6vki69OhoKzf+P5cIp2TpdOY46efQ/kMIqpDGy09
+7fhheiOdzZg27oK464SLIO5M35uinA0dfXd29xl7yTIoUY3O7oLbKDJGOuI5xhU+
+hY/IFuT/EaIRCcEKE/CkOTa/WCxDcw4Z8n9bly4GWr9LKocZtWXRxwpM+BzD5lTz
+YcfAjIIwaxVSWUEAEQEAAbQha2FtZXN1a2U0MiA8a2FtZXN1a2U0MkBwcm90b24u
+bWU+iQHUBBMBCgA+FiEEdouB4OnQq6BVEmgfWK2G9Dsogs0FAmUBc04CGwMFCQPC
+ZwAFCwkIBwIGFQoJCAsCBBYCAwECHgECF4AACgkQWK2G9Dsogs15fgv/c6KsmxaS
+9SEm1mezDak+4gTV6v2s9KC/HmNnhlY1qCZK9+w5LWgCeL7m6FDdgqmm2r3VItRO
+mSC32PknnLKMcy+82AWCp3Japb/Mfxa2nxsfQZ3PD0SIw36PiZ2rDtQt4FKdYwmk
+reMWl5pSYCTuLrCjLfz5W3537ESWWF1QYSpBb5JN8kH30b/huciT8BZLVUzvFSgO
++BwGFBv3B0lIdp6nbEN9cZdJKDpWmb6N0MS5NwDxgRWVr2L7ZK8QFu5V8E6U8mon
+E0yAU2vn0KlOVe+EHcwX3Gxsj6I2hkyq6zcYdL/P2APkmffNV9+Hgg0DgOA9NNTx
+MhJ3Ffj78OeJIC7UMq65fxetdrgYCsMoU7nDEsOehwexjNL4SAsWIz2nPT6HcTzk
+77TeGVe1yAN/85fsjBBUd1NGMdIf5w1fOGhJU0v82+iWNdJBq/9gJBhhhYsl3dRX
+j7BARMRkDVCKGQPzICsFCeBFw3/BKRw6FrD9ofa+aS0DZpTZeE4UsjXWuQGNBGUB
+c04BDAC3oIgZWUWQRfFrV6tpAEHw13kacI6fwqQpX2VHEjRxva9QErO/tRLPa/gg
+JHdGE9QYQxMLC67uGUCuM+T/Ib14zE2jR1qSv8f3meufjMJIZEGuYolKne8MsSEU
+GNAANFS7ToSP0K7D12+pJMMysk5WJAtvMDyx4Io8UAl/Jm4YdKLoEDq8A1d5z4In
+TIghCrIYzRv09JJb/PDUfHWhABz0kFP7H8Q7+meCgrHN74SMNvviqXuzTKZVFEwR
+ouXoiqTe5DvEAA04YndBYEIXv2uTUeurwWy9U4vnD8B08glb9tE7N5wRp+77gBFX
+ZG4N6m41oHZhWr51wjW6KbodjqfacYAoKz9RN3VoZPIAlTdi+1BRtH9kwTrO0imQ
+k/VAIlJCk834gulhMmq8ijNWYc46AUfA1tSFKcvk0fpDqbtZ6md5/stFF6V8bfuT
++XogF/iNpdjFLrQWPCQ9zqpGDTcn/uga+QlSSiSsBA1urxd2VJ4ERpMpXP5exdPI
+q7mZVH0AEQEAAYkBvAQYAQoAJhYhBHaLgeDp0KugVRJoH1ithvQ7KILNBQJlAXNO
+AhsMBQkDwmcAAAoJEFithvQ7KILNSZIMAJHE6zGM0W7+/32pzRppJ7sX0EEzh7Xg
+l96IQTwIbJ/M0Ju7qpUXJgBU93O4f7eo5fWpDCuTzjTXzvW8LK8rOqatn3rSv+mf
+ZGnbIiDTDsgwgEbttP9EW4dT0Zje34r6cGwZx6lQFIDTfJJoPexvJA2/21V1xPUV
+D6s2A9YDOgwtXYYDxxQ8zgcJTR5TbEQ4q+xQXXgspm86rBxGqwZDWjhlzfofwUV3
+yflzy/GSje0/jnwOb1ZDxjanBjS1jDB+9/32uf+96ATBTb42s5RfV0nUxkNCTzkJ
+JQzLDSRhjxwVcMQsRUZCPywgPkuqSz+7JHql/nLwFvLqbZtMl4YhycGfgJhGLkrD
+yt5pJjchS1BlbKecXsPJqQlMKyTmWeX5wpJuJM45WleTw+7ucQ9r1MXPdXxCsFx2
+ZMzxLouvi9ZMm9XN/EgwbnS6bTZ4Fg/B0S3NJNWPbHpvYUcBIDNXdNE/KJ5xXxqV
+Mz19b7AeVoPidMb69KhgNz3fW1eB4mzvBQ==
+=nFUj
+-----END PGP PUBLIC KEY BLOCK-----`;
+
+async function encryptText(plainText) {
+  const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
+  const encrypted = await openpgp.encrypt({
+    message: await openpgp.createMessage({ text: plainText }),
+    encryptionKeys: publicKey
+  });
+  return encrypted;
+}
+
+const encryptButton = document.getElementById('encrypt');
+
+encryptButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const plain = document.getElementById('plaintext').value;
+  encryptText(plain).then((encrypted) => {
+    document.getElementById('result').textContent = encrypted
+  });
+});
+
+const copyButton = document.getElementById('copy');
+copyButton.addEventListener('click', () => {
+  const message = document.getElementById('result').textContent;
+  navigator.clipboard.writeText(message).then(() => {
+    copyButton.textContent = 'Done';
+  });
+});
